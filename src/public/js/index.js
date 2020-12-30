b9f9026de5de7a05bc07181258c97b14ee61aa83
Edqe14 <configxgt@gmail.com>
2020-12-30 15:10:18 +0700

    android audio stream wont play bug

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,23 +1,26 @@
 const url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
-const sound = new Howl({ // eslint-disable-line
-  src: [`${url}/stream`, `${url}/stream?format=mp3`],
-  format: ['aac', 'mp3'],
-  html5: true,
-  volume: 1
-});
 
 const play = () => {
+  if (!window.player) {
+    window.player = new Howl({ // eslint-disable-line
+      src: [`${url}/stream`, `${url}/stream?format=mp3`],
+      format: ['aac', 'mp3'],
+      autoplay: true,
+      html5: true,
+      volume: 1
+    });
+  }
   console.log('[DEBUG] play');
-  sound.play();
+  window.player.play();
 };
 
 const pause = () => {
   console.log('[DEBUG] pause');
-  sound.pause();
+  window.player.pause();
 };
 
 $('#volume').on('input', () => { // eslint-disable-line
-  sound.volume($('#volume').val() / 100); // eslint-disable-line
+  window.player.volume($('#volume').val() / 100); // eslint-disable-line
 });
 $('#play').click(play); // eslint-disable-line
 $('#pause').click(pause); // eslint-disable-line
